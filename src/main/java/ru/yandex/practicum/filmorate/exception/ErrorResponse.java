cabb9668217b7a46ffc5b90436adf047deaf96c0
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-05-19 16:22:40 +0300

    Update ErrorResponse.java

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
@@ -1,12 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-
-import org.springframework.util.StringUtils;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
-
-import java.time.LocalDate;
-
 public class ErrorResponse {
     private  String error;
 
