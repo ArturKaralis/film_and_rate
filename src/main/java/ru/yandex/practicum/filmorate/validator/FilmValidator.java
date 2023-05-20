2c8dff9f937d2dc20c8578ec33ad577217124c0e
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-05-20 10:41:39 +0300

    Edit#2

--- src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
@@ -26,10 +27,10 @@ public class FilmValidator {
             log.warn("Валидация не пройдена");
             throw new ValidationException("Максимальная длина описания — 200 символов");
         }*/
-        /*if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(dateRelease)) {
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(dateRelease)) {
             log.warn("Валидация не пройдена");
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
-        }*/
+        }
         /*if (film.getDuration() <= 0) {
             log.warn("Валидация не пройдена");
             throw new ValidationException("Продолжительность фильма должна быть положительной");
