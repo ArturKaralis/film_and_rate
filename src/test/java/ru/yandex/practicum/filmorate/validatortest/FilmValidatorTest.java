4047e1a88ea69b2a35e44ea451ccccd7b21fcdda
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-05-29 19:44:50 +0300

    Исключили тесты на время

--- src/test/java/ru/yandex/practicum/filmorate/validatortest/FilmValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validatortest/FilmValidatorTest.java
@@ -1,14 +1,6 @@
 package ru.yandex.practicum.filmorate.validatortest;
 
 
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.validator.FilmValidator;
-
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 public class FilmValidatorTest {
 
 
--- src/test/java/ru/yandex/practicum/filmorate/validatortest/UserValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validatortest/UserValidatorTest.java
@@ -1,14 +1,6 @@
 package ru.yandex.practicum.filmorate.validatortest;
 
 
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.validator.UserValidator;
-
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 class UserValidatorTest {
 
 
