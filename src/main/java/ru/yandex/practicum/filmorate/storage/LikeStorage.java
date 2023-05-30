dbb18723ed65ac741af10126bc258b09ec777100
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-05-30 14:02:01 +0300

    кодстайл :(

--- src/main/java/ru/yandex/practicum/filmorate/storage/LikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/LikeStorage.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface LikeStorage {
 
     List<Film> getTopFilms(int count);
+
     void addLike(Long id, Long userId);
 
     void removeLike(Long id, Long userId);
